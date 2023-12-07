17db45d8d483f65bd27428ff718894487aef6c7f
Leonardo Holanda <leo.uldholanda@gmail.com>
2023-12-07 13:22:26 -0300

    Add quiz component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { QuizComponent } from './quiz/quiz.component';
 
 @Component({
   selector: 'bfd-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, QuizComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
---
+++ src/app/quiz/quiz.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bfd-quiz',
+  standalone: true,
+  imports: [],
+  templateUrl: './quiz.component.html',
+  styleUrl: './quiz.component.scss',
+})
+export class QuizComponent {}
