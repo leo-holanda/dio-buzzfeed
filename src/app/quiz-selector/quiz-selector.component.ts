7be7ee03b3c64f6f570bc199062cbe7b457926e6
Leonardo Holanda <leo.uldholanda@gmail.com>
2023-12-07 13:41:34 -0300

    Add quiz selector component

---
+++ src/app/quiz-selector/quiz-selector.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bfd-quiz-selector',
+  standalone: true,
+  imports: [],
+  templateUrl: './quiz-selector.component.html',
+  styleUrl: './quiz-selector.component.scss',
+})
+export class QuizSelectorComponent {}
