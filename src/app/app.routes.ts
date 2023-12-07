e36c29e83512727041d5dda4f6682abe5850e024
Leonardo Holanda <leo.uldholanda@gmail.com>
2023-12-07 13:42:51 -0300

    Add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { QuizSelectorComponent } from './quiz-selector/quiz-selector.component';
+import { QuizComponent } from './quiz/quiz.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: QuizSelectorComponent },
+  { path: 'quiz', component: QuizComponent },
+];
