abd7c1eab4fd0bc2c6dddcc359787e0cfc0b976f
Leonardo Holanda <leo.uldholanda@gmail.com>
2023-12-07 13:40:20 -0300

    Add quiz model

---
+++ src/app/quiz/quiz.model.ts
@@ -0,0 +1,16 @@
+export type Answer = {
+  text: string;
+  outcome: string;
+};
+
+export type Question = {
+  statement: string;
+  answers: Answer[];
+};
+
+export type Quiz = {
+  title: string;
+  questions: Question[];
+  outcomes: string[];
+  profiling: (answers: Answer[]) => string;
+};
